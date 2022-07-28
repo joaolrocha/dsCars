e50fd0f9da8fd778fd34cddd8da08a8ae17f8efc
joaolrocha <joaolrocha07@gmail.com>
2022-07-28 11:41:27 -0300

    :sparkles: global styles add

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,14 @@
 import './assets/styles/custom.scss';
 
 
+import Navbar from 'components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <h1>Carros Top</h1>
+    <>
+    <Navbar />
+    </>
   );
 }
 
