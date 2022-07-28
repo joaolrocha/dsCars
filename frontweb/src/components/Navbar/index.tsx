611d8ad3fcf607228505d97728e92f67075e9c2c
joaolrocha <joaolrocha07@gmail.com>
2022-07-28 12:35:13 -0300

    :sparkles: navbar styling

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import 'bootstrap/js/src/collapse.js';
 import './styles.css';
 
 
@@ -8,7 +9,20 @@ const Navbar = () => {
         <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
-        <div className='collapse navbar-collapse'>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className='collapse navbar-collapse' id='dscatalog-navbar'>
           <ul className='navbar-nav offset-md-2 main-menu'>
             <li>
               <a href="link" className='active'>HOME</a>
